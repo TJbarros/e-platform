07bc4c6792860fde813bd539aebda623a8a0bb27
TJbarros <tjabarros@hotmail.com>
2019-03-03 15:57:47 -0300

    Ajustando links do layout de atenticação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,12 +8,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Favicon -->
-    <link rel="icon" href="{{url('assets/img/icon.jpg')}}">
+    <link rel="icon" href="{{secure_asset('assets/img/icon.jpg')}}">
 
     <title>E-Platform</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -77,6 +77,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
