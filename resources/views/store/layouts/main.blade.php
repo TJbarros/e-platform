1bdec38b6067955b7d68168f82240f2f8a668771
TJbarros <tjabarros@hotmail.com>
2019-03-04 15:32:49 -0300

    Adicionando CDN fontawesome

--- resources/views/store/layouts/main.blade.php
+++ resources/views/store/layouts/main.blade.php
@@ -10,6 +10,7 @@
     <!-- Favicon -->
     <link rel="icon" href="{{ secure_asset('assets/img/icon.jpg')}}">
     <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link href="{{ secure_asset('assets/style.css')}}" rel="stylesheet">
     <!-- Responsive CSS -->
     <link href="{{ secure_asset('assets/css/responsive.css')}}" rel="stylesheet">
